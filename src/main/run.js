008b101efe16dba00728a3a0bd1ab7e8d94515b7
shaoshilei <laoono@163.com>
2020-06-08 17:45:25 +0800

    feat: 支持加载默认配置文件

--- src/main/run.js
+++ src/main/run.js
@@ -11,24 +11,35 @@ const fs = require('fs');
 const {log} = require('../lib');
 const path = require('path');
 const vmjs = require('./vm');
+const merge = require('lodash/merge');
 
 module.exports = function(configPath, env) {
 
-  const configJs = path.join(configPath, `./config/${env}.js`);
+  const configJs = path.join(configPath, `./config/${env}.js`); // 区分环境的配置文件
+  const defaultConfigJs = path.join(configPath, './config/default.js'); // 默认的配置文件
 
-  if (!fs.existsSync(configJs)) {
-    log.error('没有找到配置文件', configJs);
+  if (!fs.existsSync(defaultConfigJs)) {
+    log.error('没有找到配置文件', defaultConfigJs);
     process.exit();
     return null;
   }
 
-  const code = fs.readFileSync(configJs, 'utf-8');
+  // 配置文件名称
   const configFileName = 'config';
-  const config = vmjs(code, configJs);
+
+  // 根据读取的默认配置文件内容转化成javascript对象
+  const defaultConfig = vmjs(fs.readFileSync(defaultConfigJs, 'utf-8'), defaultConfigJs);
+
+  // 如果存在区分环境的配置文件，取出文件内容转成javascript对象
+  const config = fs.existsSync(configJs)
+  ? vmjs(fs.readFileSync(configJs, 'utf-8'), configJs)
+  : {};
 
   const srcConfig = path.join(configPath, `./src/${configFileName}.js`);
 
-  const content = `module.exports = ${JSON.stringify(config)}`;
+  // 合并配置文件代码内容
+  // 环境配置文件会覆盖默认配置文件的同名配置
+  const content = `module.exports = ${JSON.stringify(merge({}, defaultConfig, config))}`;
 
   new File(srcConfig, content).save();
 };
