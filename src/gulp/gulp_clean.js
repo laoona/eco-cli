8e99e57aedd522e7067d2fba8952fd4fd7a489e3
shaoshilei <laoono@163.com>
2020-06-11 16:37:13 +0800

    feat: 删除经过base64的图片资源

--- src/gulp/gulp_clean.js
+++ src/gulp/gulp_clean.js
@@ -7,11 +7,52 @@
  */
 
 const gulp = require('gulp');
-const debug = require('gulp-debug');
-const clean = require('gulp-clean');
+const through = require('through2');
+const path = require('path');
+const del = require('del');
+const {log} = require('../lib');
+
+const fs = require('fs');
+const pwd = process.cwd();
+
+/**
+ * 删除样式表里经过base64的图片资源
+ */
+const delImageByBase64 = () => {
+  return through.obj(function (file, enc, cb) {
+
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
+
+    if (file.isStream()) {
+      this.emit('error', 'Streaming not supported');
+    }
+
+    // 匹配文件内容中含有. ?base64的路径
+    const matches = file.contents.toString().match(/\..*\?base64/g);
+  
+    if (matches && matches.length) {
+      matches.forEach(img => {
+        let loc = path.join(path.dirname(file.path), img);
+
+        // 替换路径中的src为dist
+        loc = loc.replace(/\?base64$/g, '').replace(`${pwd}/src/`, `${pwd}/dist/`);
+
+        if (!fs.existsSync(loc)) return;
+
+        // 删除掉
+        del(loc);
+        log.info(`删除base64的源文件：${loc.replace(`${pwd}/dist/`, '')}`);
+      });
+    }
+
+    return cb();
+  });
+}
 
 module.exports = function () {
-  return gulp.src(['dist/**/_*.png', 'dist/**/_*.jpg', 'dist/**/_*.jpeg', 'dist/**/_*.gif'])
-    .pipe(debug({title: 'image'}))
-    .pipe(clean({force: true, read: false}));
+  return gulp.src(['src/**/*.wxss', 'src/**/*.scss'])
+    .pipe(delImageByBase64())
 };
